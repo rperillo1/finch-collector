2de8ea82aa491e8b9a6a37271bfa2198ba4e55a1
Robert Perillo <perillo.robert@gmail.com>
2020-06-16 16:18:00 -0600

    add about and index page

--- main_app/views.py
+++ main_app/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def finches_index(request):
+    return render(request, 'finches/index.html')
